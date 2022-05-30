a9c2881fe3f5f3b71ec0bb83dcbc30f46ebd669e
samuel2002Es <samuel2002espinoza@gmail.com>
2022-05-30 18:07:43 -0500

    terminacion del curso

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ JavaScript es el lenguaje más utilizado para desarrollo de aplicaciones web, pr
 * Entender qué es ECMAScript y por qué es tan importante
 
 En este curso se abordo desde el ecmascript 6 hasta el mas actual, ademas a destacar se aprendio que son las promesas en js
+
+![Image text](https://static.platzi.com/media/user_upload/JavaScript%20ECMA-cb68fa75-9797-4604-bf0c-ad083aad6d3c.jpg)
--- src/es12/index.js
+++ src/es12/index.js
@@ -1,3 +1,59 @@
-const string = ' Javascript es maravilloso, con el puedo crear el furutro de la web';
-const replacedString = string.replaced("Javascript", "Python");
-console.log(string);
+// 🧯 Replace
+
+const string = "JavaScript es maravilloso, con JavaScript puedo crear el futuro de la web.";
+const replacedString = string.replace("JavaScript", "Python");
+console.log(replacedString);
+
+// Output: Python es maravilloso, con JavaScript puedo crear el futuro de la web.
+
+const replacedString2 = string.replaceAll("JavaScript", "Python");
+console.log(replacedString2);
+
+// Output: Python es maravilloso, con Python puedo crear el futuro de la web.
+
+// 🔒 Metodos privados
+class Message {
+    #show(val){ // Con el # convertimos al método en privado
+        console.log(val);
+    };
+};
+
+const message = new Message();
+message.show('Hola!');
+
+//  👽 Promise Any
+/* el primer valor que sea resuelto retorna y termina la ejecucion con la utilizacion de any */
+const promise1 = new Promise((resolve, reject) => reject("1"));
+const promise2 = new Promise((resolve, reject) => resolve("2"));
+const promise3 = new Promise((resolve, reject) => resolve("3"));
+
+Promise.any([promise1, promise2, promise3])
+    .then(response => console.log(response));
+
+
+//  🦴 WeakRef(element);
+/* para que no sea recogido por la memoria */
+class AnyClass {
+    constructor(element){
+        this.ref = new WeakRef
+    }
+    {...}
+}
+
+// 🧪 Nuevos operadores lógicos
+
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue &&= isFalse);
+
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue ||= isFalse);
+
+let isTrue = undefined;
+let isFalse = false;
+console.log(isTrue ??= isFalse);
+
+let b ='a'; 
+b ='b';
+console.log(b);
